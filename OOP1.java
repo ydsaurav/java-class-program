95aa84ce751674243bf2770c5452763af6d18f53
SAURAV <55505772+ydsaurav@users.noreply.github.com>
2021-04-08 13:22:40 +0530

    Add files via upload

---
+++ OOP1.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+/*
+class Student
+{
+    private int id;
+    private String name;
+    private String course;
+    private double fees;
+    
+    public void FeedData(){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Id : ");
+        id = sc.nextInt();
+        
+        sc.nextLine();
+        
+        System.out.print("Name : ");
+        name = sc.nextLine();
+        
+        System.out.print("Course : ");
+        course = sc.nextLine();
+        
+        System.out.println("Fees  : ");
+        fees = sc.nextDouble();
+    }
+    
+    public void ShowData(){
+        System.out.println("Id : " + id);
+        System.out.println("Name : " + name);
+        System.out.println("Course : " + course);
+        System.out.println("Fees : " + fees);
+    }
+}
+
+public class OOP1 {
+    public static void main(String []args){
+        Student stu1 = new Student();
+        stu1.FeedData();
+        stu1.ShowData();
+        
+        System.out.println("---------------------------------------------");
+        Student stu2 = new Student();
+        stu2.FeedData();
+        stu2.ShowData();
+    }
+}
+*/
